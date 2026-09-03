d050dbcff61241215524ff2be718b0cf8527abde
Gervin Õun <gervinoun@gmail.com>
2026-09-03 09:50:20 +0300

    add math examples

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
 $test = 10;
-$test = 3.1;
-$test = "Hello World";
-$test = true;
-$test = false;
-$test = null;
-$test = [1, null, true, 'lol', 2, 3.2];
-$test = new stdClass();
-$test->name = "Gervin";
-var_dump($test);
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10;
+$test = 10 / 10;
+$test = $test + 10;
+$test = 2 ** 3;
+$test = 5 % 2;
+$test += 3;
+$test -= 3;
+$test *= 3;
+$test /= 3;
+$test %= 3;
+$test **= 3;
+$test %= 3;
+$test++;
+$test--;
+--$test;
+++$test;
+
+$a = 1;
+$b = ++$a;
+var_dump($b, $a);
+
+$test = sin(pi())
 ?>
