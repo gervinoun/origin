f896aa36fac3e643da8c6aee2407c2b6feffd2be
Gervin Õun <gervinoun@gmail.com>
2026-09-03 09:38:35 +0300

    Add type examples

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$test = 10;
+$test = 3.1;
+$test = "Hello World";
+$test = true;
+$test = false;
+$test = null;
+$test = [1, null, true, 'lol', 2, 3.2];
+$test = new stdClass();
+$test->name = "Gervin";
+var_dump($test);
+?>
