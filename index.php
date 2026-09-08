eb9577f00080fea670786a46dad69ca61c3d6c68
Gervin Õun <gervinoun@gmail.com>
2026-09-08 09:23:30 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 < 10;
-$test = 10 == 10;
-$test = 10 >= 10;
-$test = 10 <= 10;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = 10 != '10';
-$test = 10 !== '10';
-$test = 0 == false;
+$test = array(1, 2, 3);
+$test = [1, 2, 3];
+$test = [1 , 'asdas', true, [1, 2, 3]];
+$test =  [
+    1,
+    'name' => 'Poeg Nutu_emoji',
+    'age' => 12,
+    3,
+    'saues',
+    true,
+    100 => 'lol',
+    'troll',
+];
+//var_dump($test[3][1]);
+var_dump($test['name']);
+array_push($test, 'added', 'zzaxszx<zxa', 3);
+$test[] = 'wallahi tauri' ;
+$test['name'] = 'Jõgeva';
+$test[1] = 99999; 
+unset($test[3]);
 var_dump($test);
